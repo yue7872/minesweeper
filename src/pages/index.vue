ea8beb09b4309866df84a5f83c97df94e5d2f250
blairyue <blairyue@tencent.com>
2022-03-29 23:29:24 +0800

    feat: ✨基本功能实现 。

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,53 +3,136 @@ interface MineBlock {
   x: number
   y: number
   boom?: boolean
+  open?: boolean
+  mineNum: number
 }
+const WIDTH = 10
+const HEIGHT = 10
 
-const WIDTH = 5
-const HEIGHT = 5
-
-function btnClick(items: MineBlock) {
-  console.log(items)
+const permission = reactive({
+  disabled: false,
+  dev: false,
+})
+const state: MineBlock[][] = reactive(
+  Array.from({ length: WIDTH }, (_, x) => {
+    return Array.from({ length: WIDTH }, (_, y): MineBlock => {
+      return { x, y, mineNum: 0, open: false, boom: false }
+    })
+  }),
+)
+function expendZero(items: MineBlock) {
+  if (items.mineNum !== 0)
+    return
+  const direction = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ]
+  items.open = true
+  direction.forEach(([dx, dy]) => {
+    const x2 = items.x + dx
+    const y2 = items.y + dy
+    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+      return
+    if (state[x2][y2].open === false) {
+      state[x2][y2].open = true
+      expendZero(state[x2][y2])
+    }
+  })
 }
-
-function generateMine(x: number, y: number) {
-  const allMine: MineBlock[][] = Array.from(Array(x), () => [])
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-      const block: MineBlock = {
-        x: i,
-        y: j,
-        boom: Math.random() < 0.1,
-      }
-      allMine[i].push(block)
+function getMineClass(items: MineBlock) {
+  return items.open ? '' : 'closed'
+}
+function cheat() {
+  permission.dev = !permission.dev
+}
+function generateMine() {
+  permission.disabled = false
+  for (const line of state) {
+    for (const block of line) {
+      block.open = false
+      block.mineNum = 0
+      block.boom = Math.random() < 0.1
     }
   }
-  return allMine
+  calculateNum()
+}
+function btnClick(items: MineBlock) {
+  if (items.boom) {
+    state.forEach((line) => {
+      line.forEach((block) => {
+        if (block.boom) {
+          permission.disabled = true
+          block.open = true
+        }
+      })
+    })
+    alert('boom!')
+    return
+  }
+  items.open = true
+  expendZero(items)
 }
 
-const allMine = generateMine(WIDTH, HEIGHT)
+generateMine()
 
+function calculateNum() {
+  const direction = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+    [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
+  ]
+  for (const line of state) {
+    for (const block of line) {
+      direction.forEach(([dx, dy]) => {
+        const x2 = block.x + dx
+        const y2 = block.y + dy
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+          return
+        if (state[x2][y2].boom)
+          block.mineNum++
+      })
+    }
+  }
+}
 </script>
 
 <template>
   <div>
     <div
-      v-for="(line,index) in allMine"
+      v-for="(line,index) in state"
       :key="index"
       class="box"
     >
       <button
-        v-for="(items,i) in allMine[index]"
+        v-for="(items,i) in line"
         :key="i"
         class="button"
-        @click="btnClick(items)"
+        :class="getMineClass(items) + (items.boom ? ' boom' : '')"
+        :disabled="permission.disabled ? true : false"
+        @click="btnClick(items,state)"
       >
-        {{ items.boom ? 'X' : '' }}
+        {{ (permission.dev || items.open) ? items.boom ? 'X' : items.mineNum : '' }}
       </button>
     </div>
+    <button class="bottombtn" @click="cheat()">
+      cheat: {{ permission.dev ? 'on' : 'off' }}
+    </button>
+    <button class="bottombtn" @click="generateMine()">
+      reset
+    </button>
   </div>
 </template>
 <style>
+.page {
+  margin-bottom: 20px;
+}
 .box {
   display: flex;
   flex-wrap: no-wrap;
@@ -62,7 +145,24 @@ const allMine = generateMine(WIDTH, HEIGHT)
   display: inline-block;
   width: 50px;
   height: 50px;
-  border: 1px solid red;
-  margin-left: -1px;
+  margin: 1px;
+  color: white;
+  background: #669933;
 }
+.button.boom {
+  background: #CC3333;
+}
+.button.closed {
+  background: #003366;
+}
+.bottombtn {
+  border-radius: 5px;
+  margin: 20px auto;
+  width: 30%;
+  display: block;
+  background: #00bcd4;
+  height: 50px;
+  color: white;
+}
+
 </style>
